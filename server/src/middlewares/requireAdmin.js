a1b9cb237b0829fcc9f254806094fc4e4dd73ae7
Shen <shen.zhang0907@gmail.com>
2026-03-11 16:29:54 -0700

    little fix

--- server/src/middlewares/requireAdmin.js
+++ server/src/middlewares/requireAdmin.js
@@ -2,6 +2,10 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function requireAdmin(req, res, next) {
   try {
+    console.log("AUTH origin:", req.headers.origin);
+    console.log("AUTH cookie header:", req.headers.cookie);
+    console.log("AUTH parsed cookies:", req.cookies);
+    console.log("AUTH admin_token:", req.cookies?.admin_token);
     const token = req.cookies?.admin_token;
     if (!token) return res.status(401).json({ error: "Unauthorized" });
 
