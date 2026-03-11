19d2d5876ce36d20714eaeb586f04b9166744d5a
Shen <shen.zhang0907@gmail.com>
2026-03-11 15:07:12 -0700

    More adjsutment for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://bristar-ecommerce-platform-87rehhvbg.vercel.app/",
+      "https://bristar-ecommerce-platform-87rehhvbg.vercel.app",
     ],
     credentials: true,
   }),
